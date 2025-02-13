860752002fbf9461f80da468b8103b81f8843675
firstPenguin1988 <tusharparmarofficial@gmail.com>
2025-02-13 17:25:45 +0530

    HomePage and page.tsx changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,7 @@
-import Navbar from "@/components/Navbar";
-import CraftingUniqueConnection from "@/sections/CraftingUniqueConnection/CraftingUniqueConnection";
-import HeroSection from "@/sections/HeroSection/HeroSection";
-import PeopleJobSection from "@/sections/PeopleJobSection/PeopleJobSection";
-import TestimonialSection from "@/sections/TestimonialSection/TestimonialSection";
-import TrustedBrand from "@/sections/TrustedBrand/TrustedBrand";
-import ratingImg from '../../public/homepage/fiveStar.webp'
-import FuelingHealthcareSection from "@/sections/FuelingHealthcareSection/FuelingHealthcareSection";
-import ContactUs from "@/sections/ContactUs/ContactUs";
-import Footer from "@/sections/Footer/Footer";
-
-const testimonials = [
-  {
-    name: 'Deepak Sahu',
-    title: 'Stealth VC Fund',
-    ratingImg: ratingImg,
-    review: "“Xponentiate is the best recruitment team I've ever partnered with.”"
-  },
-  {
-    name: 'Isaac Newton',
-    title: 'Stealth VC Fund',
-    ratingImg: ratingImg,
-    review: "“Xponentiate is the best recruitment team I've ever partnered with. Xponentiate is the best recruitment team I've ever partnered with.”"
-  },
-  {
-    name: 'Elbert Einstein',
-    title: 'Stealth VC Fund',
-    ratingImg: ratingImg,
-    review: "“Xponentiate is the best recruitment team I've ever partnered with.Xponentiate is the best recruitment team I've ever partnered with.Xponentiate is the best recruitment team I've ever partnered with.”"
-  },
-  {
-    name: 'Thomas Edison',
-    title: 'Stealth VC Fund',
-    ratingImg: ratingImg,
-    review: "“Xponentiate is the best recruitment team I've ever partnered with.Xponentiate is the best recruitment.”"
-  },
-]
+import HomePage from "@/sections/HomePage/HomePage";
 
 export default function Home() {
+  
   return (
     <div
       className="h-screen"
@@ -44,15 +9,7 @@ export default function Home() {
         background: "linear-gradient(to bottom, #F9F9F9 53%, #ECEBEB 47%)"
       }}
     >
-      <Navbar />
-      <HeroSection />
-      <TrustedBrand />
-      <CraftingUniqueConnection />
-      <PeopleJobSection />
-      <TestimonialSection testimonials={testimonials} />
-      <FuelingHealthcareSection />
-      <ContactUs />
-      <Footer />
+     <HomePage />
     </div>
   );
 }
---
+++ src/sections/HomePage/HomePage.tsx
@@ -0,0 +1,60 @@
+'use client'
+import React from 'react'
+import Navbar from "@/components/Navbar";
+import CraftingUniqueConnection from "@/sections/CraftingUniqueConnection/CraftingUniqueConnection";
+import HeroSection from "@/sections/HeroSection/HeroSection";
+import PeopleJobSection from "@/sections/PeopleJobSection/PeopleJobSection";
+import TestimonialSection from "@/sections/TestimonialSection/TestimonialSection";
+import TrustedBrand from "@/sections/TrustedBrand/TrustedBrand";
+import ratingImg from '../../../public/homepage/fiveStar.webp'
+import FuelingHealthcareSection from "@/sections/FuelingHealthcareSection/FuelingHealthcareSection";
+import ContactUs from "@/sections/ContactUs/ContactUs";
+import Footer from "@/sections/Footer/Footer";
+import RightChoiceSection from "@/sections/RightChoiceSection/RightChoiceSection";
+
+const testimonials = [
+    {
+        name: 'Deepak Sahu',
+        title: 'Stealth VC Fund',
+        ratingImg: ratingImg,
+        review: "“Xponentiate is the best recruitment team I've ever partnered with.”"
+    },
+    {
+        name: 'Isaac Newton',
+        title: 'Stealth VC Fund',
+        ratingImg: ratingImg,
+        review: "“Xponentiate is the best recruitment team I've ever partnered with. Xponentiate is the best recruitment team I've ever partnered with.”"
+    },
+    {
+        name: 'Elbert Einstein',
+        title: 'Stealth VC Fund',
+        ratingImg: ratingImg,
+        review: "“Xponentiate is the best recruitment team I've ever partnered with.Xponentiate is the best recruitment team I've ever partnered with.Xponentiate is the best recruitment team I've ever partnered with.”"
+    },
+    {
+        name: 'Thomas Edison',
+        title: 'Stealth VC Fund',
+        ratingImg: ratingImg,
+        review: "“Xponentiate is the best recruitment team I've ever partnered with.Xponentiate is the best recruitment.”"
+    },
+]
+
+const HomePage = () => {
+ 
+    return (
+        <>
+            <Navbar />
+            <HeroSection />
+            <TrustedBrand />
+            <CraftingUniqueConnection />
+            <PeopleJobSection />
+            <RightChoiceSection />
+            <TestimonialSection testimonials={testimonials} />
+            <FuelingHealthcareSection />
+            <ContactUs />
+            <Footer />
+        </>
+    )
+}
+
+export default HomePage
