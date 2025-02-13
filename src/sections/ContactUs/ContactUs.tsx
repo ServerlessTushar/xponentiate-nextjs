92be83b063bcc15e4482ac93d86956660cc31dfa
firstPenguin1988 <tusharparmarofficial@gmail.com>
2025-02-13 19:49:04 +0530

    contact us changed

--- src/sections/ContactUs/ContactUs.tsx
+++ src/sections/ContactUs/ContactUs.tsx
@@ -27,9 +27,7 @@ export default function ContactUs() {
 
           {/* Form Section */}
           <div className="w-full lg:w-1/2">
-       
               <Form />
-        
           </div>
         </div>
       </div>
